67dedee7e3812f4dd9a68ee7c8c3419dcd5a4a98
Olga F <krasota1_00@mail.ru>
2023-02-13 20:30:38 +0700

    changing type names

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 2000;  //сумма на текущем счете клиента
-        int b = 1001;  // сумма пополнения
-        int c = 100;  // стоимость 1 подарочного рубля
-        if (b >= 1001) {
-            System.out.println("Количество бонусных рублей" + (b / c));
+        int score = 2000;  //сумма на текущем счете клиента
+        int replenishment = 1001;  // сумма пополнения
+        int gift = 100;  // стоимость 1 подарочного рубля
+        if (replenishment >= 1001) {
+            System.out.println("Количество бонусных рублей" + (replenishment / gift));
         } else {
             System.out.println("Количество бонусных рублей" + 0);
         }
-        System.out.println("Итоговый счет" + (a + b));
+        System.out.println("Итоговый счет" + (score + replenishment));
     }
 }
